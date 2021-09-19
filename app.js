023298c9786b45490a3f254f0e69592c1497a70c
Damian <damian.ullmann@gmail.com>
2021-09-19 20:07:52 -0300

    Desafio 1 y 2

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ y = prompt("Ingrese un numero");
 if (isNaN(x) || isNaN(y)) {
     alert("Es necesarios introducir dos números válidos");
 } else {
-    //si no ponemos parseFloat concatenaría x con y
     suma = parseFloat(x) + parseFloat(y);
     alert("El resultado de la suma es " + suma);
 }
