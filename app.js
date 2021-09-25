137d37d3ac5fa2365fc8f47e881f238e231f185e
damian9668 <54315908+damian9668@users.noreply.github.com>
2021-09-25 11:58:36 -0300

    desafio ciclos

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let resultado = 0;
 
 for (let x=0; x=4;x++){
 
-    let i = prompt("ingrese un numero");
+    let i = prompt("ingrese un numero, para terminar ingresa 'esc'");
     if(i == "esc"){
         break;
     };
