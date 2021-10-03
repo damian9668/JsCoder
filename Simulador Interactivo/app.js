561d927f0dfae6d124ee711a5f41e2716d6cc0ce
damian9668 <54315908+damian9668@users.noreply.github.com>
2021-10-03 18:56:32 -0300

    bug fix

--- Simulador Interactivo/app.js
+++ Simulador Interactivo/app.js
@@ -7,6 +7,6 @@ let resultado =0;
 credito(monto,cuotas,interes);
 
 function credito (monto,cuotas,interes){
-    resultado= parseFloat( (parseFloat(monto) * ((parseFloat(interes) + 100)/100))/cuotas).toFixed(2);
+    resultado=((parseFloat(monto) * ((parseFloat(interes) + 100)/100))/cuotas).toFixed(2);
     alert("usted pagara una cuota fija de: " + resultado);
 }
