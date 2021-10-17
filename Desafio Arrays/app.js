7416aa720ae118dffe7c111c9f6f319bb8e397c6
damian9668 <54315908+damian9668@users.noreply.github.com>
2021-10-17 00:48:36 -0300

    arrays

---
+++ Desafio Arrays/app.js
@@ -0,0 +1,10 @@
+
+class Credito{
+    constructor(montoTotal, interesTotal, cantidadCuotas){
+        this.montoTotal = montoTotal;
+        this.interesTotal = interesTotal;
+        this.cantidadCuotas = cantidadCuotas;
+    }
+}
+
+const creditos = new Credito()
