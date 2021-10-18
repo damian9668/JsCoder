af03b40d1bbdb785bb6ef76a6e37856bff1dc55e
Damian <damian.ullmann@gmail.com>
2021-10-18 00:43:42 -0300

    desafio DOM

---
+++ Desafio DOM/app.js
@@ -0,0 +1,37 @@
+class Alumno{
+    constructor(nombre, fechaDeNacimiento, notas ){
+        this.nombre = nombre.toUpperCase();
+        this.fecha = fechaDeNacimiento;
+        this.notas = notas;
+    }
+}
+
+const alumnos = [];
+
+const create = (nombre, fechaDeNacimiento, notas)=>{
+    alumnos.push(new Alumno(nombre, fechaDeNacimiento, notas));
+}
+
+
+function cargarAlumnos(){
+    let nombre = document.getElementById("nombre").value;
+    let fecha = document.getElementById("fecha").value;
+    let notas = document.getElementById("notas").value;
+
+    notas = notas.split(",")
+
+    create(nombre, fecha, notas);
+    console.log(alumnos);
+    document.getElementById("formulario").reset();
+}
+
+function verAlumnos(){
+    alumnos.forEach((alumno)=>{
+        let item = document.createElement("li");
+        item.innerHTML = JSON.stringify(alumno);
+        document.getElementById("mostrar").appendChild(item);
+    });
+}
+
+
+
